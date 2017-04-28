570d8c5959f8b9c2384dc1ccf4584ac278a1cc65
Haoqing Luan <hluan@andrew.cmu.edu>
2017-04-28 16:08:33 -0400

    Implement a randomly generate board and gems function, and the size could be choosed by guests

---
+++ Assets/GemGeneretor.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GemGeneretor : MonoBehaviour {
+    /*Public Variable*/
+    public GameObject gemPrefab;
+    public Material[] Materials;
+    public int sizeOfBoard;
+    /*Private Variable*/
+    private int GemTypeNumber;
+
+
+	void Start () {
+        /*Generate a game board with different gems*/
+        GemTypeNumber = Materials.Length;
+        this.transform.localScale = new Vector3((float)(sizeOfBoard / 10f), (float)(sizeOfBoard / 10f), (float)(sizeOfBoard / 10f));
+        for (int i=0;i<sizeOfBoard;i++)
+        {
+            for(int j=0;j<sizeOfBoard;j++)
+            {
+                GameObject tmp = Instantiate(gemPrefab);
+                int randNum = Random.Range(0, GemTypeNumber);
+                tmp.GetComponent<InitialBall>().Initial(Materials[randNum], randNum);
+                /*Adjust position according to the boardsize*/
+                tmp.transform.position = new Vector3((-sizeOfBoard /2)+0.5f+j, (sizeOfBoard / 2)-i-0.5f,0);
+            }
+        }
+	}
+	
+
+	void Update () {
+		
+	}
+}
---
+++ Assets/InitialBall.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InitialBall : MonoBehaviour {
+    /*Public variable*/
+
+    /*0 for Red, 1 for blue, 2 for green, 3 for yellow*/
+    public int type;
+    public float shownSpeed;
+
+    /*Private variable*/
+    private bool _isPlayAnimation = false;
+    private float _originScale;
+
+	
+	void Start () {
+        _originScale = this.transform.localScale.x;
+        this.transform.localScale = new Vector3(0.01f, 0.01f, 0.01f);
+    }
+	
+	
+	void Update () {
+        if (!_isPlayAnimation)
+        {
+            this.transform.localScale *= Time.deltaTime * shownSpeed*60;
+            if(this.transform.localScale.x>=_originScale)
+            {
+                this.transform.localScale = new Vector3(_originScale, _originScale, _originScale);
+                _isPlayAnimation = true;
+            }
+        }
+
+	}
+    /*Initial the ball with type and material*/
+    public void Initial(Material _material,int _type)
+    {
+        this.GetComponent<MeshRenderer>().material = _material;
+        this.type = _type;
+    }
+}
